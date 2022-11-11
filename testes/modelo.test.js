622be1772ac8f13a4da2fda7f484b9d9f848fa73
mtov <mtvalente@gmail.com>
2022-11-11 13:16:17 -0300

    update

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -2,7 +2,6 @@ const bd = require('../bd/bd_utils.js');
 const modelo = require('../modelo.js');
 
 beforeEach(() => {
-  // banco de dados de teste
   bd.reconfig('./bd/esmforum-teste.db');
   // limpa dados de todas as tabelas
   bd.exec('delete from perguntas', []);
@@ -13,7 +12,7 @@ test('Testando banco de dados vazio', () => {
   expect(modelo.listar_perguntas().length).toBe(0);
 });
 
-test('Testando uma lista com três perguntas', () => {
+test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
@@ -22,4 +21,5 @@ test('Testando uma lista com três perguntas', () => {
   expect(perguntas[0].texto).toEqual('1 + 1 = ?');
   expect(perguntas[1].texto).toEqual('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
